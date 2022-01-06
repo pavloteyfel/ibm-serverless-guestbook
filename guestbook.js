134f61f3c0a5118f9b79329cdc9f0c6d70b88385
pavloteyfel <pavloteyfel@gmail.com>
2022-01-06 14:00:30 +0100

    style: Add style

--- guestbook.js
+++ guestbook.js
@@ -1,78 +1,83 @@
 /**
  * Web application
  */
-const apiUrl = 'https://f6cf0b27.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = "https://f6cf0b27.eu-gb.apigw.appdomain.cloud/guestbook";
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
-      type: 'GET',
+      type: "GET",
       url: `${apiUrl}/entries`,
-      dataType: 'json'
+      dataType: "json",
     });
   },
   // add a single guestbood entry
   add(name, email, comment) {
-    console.log('Sending', name, email, comment)
+    console.log("Sending", name, email, comment);
     return $.ajax({
-      type: 'PUT',
+      type: "PUT",
       url: `${apiUrl}/entries`,
-      contentType: 'application/json; charset=utf-8',
+      contentType: "application/json; charset=utf-8",
       data: JSON.stringify({
         name,
         email,
         comment,
       }),
-      dataType: 'json',
+      dataType: "json",
     });
-  }
+  },
 };
 
-(function() {
-
+(function () {
   let entriesTemplate;
 
   function prepareTemplates() {
-    entriesTemplate = Handlebars.compile($('#entries-template').html());
+    entriesTemplate = Handlebars.compile($("#entries-template").html());
   }
 
   // retrieve entries and update the UI
   function loadEntries() {
-    console.log('Loading entries...');
-    $('#entries').html('Loading entries...');
-    guestbook.get().done(function(result) {
-      if (!result.entries) {
-        return;
-      }
+    console.log("Loading entries...");
+    $("#entries").html("Loading entries...");
+    guestbook
+      .get()
+      .done(function (result) {
+        if (!result.entries) {
+          return;
+        }
 
-      const context = {
-        entries: result.entries
-      }
-      $('#entries').html(entriesTemplate(context));
-    }).error(function(error) {
-      $('#entries').html('No entries');
-      console.log(error);
-    });
+        const context = {
+          entries: result.entries,
+        };
+        $("#entries").html(entriesTemplate(context));
+      })
+      .error(function (error) {
+        $("#entries").html("No entries");
+        console.log(error);
+      });
   }
 
   // intercept the click on the submit button, add the guestbook entry and
   // reload entries on success
-  $(document).on('submit', '#addEntry', function(e) {
+  $(document).on("submit", "#addEntry", function (e) {
     e.preventDefault();
 
-    guestbook.add(
-      $('#name').val().trim(),
-      $('#email').val().trim(),
-      $('#comment').val().trim()
-    ).done(function(result) {
-      // reload entries
-      loadEntries();
-    }).error(function(error) {
-      console.log(error);
-    });
+    guestbook
+      .add(
+        $("#name").val().trim(),
+        $("#email").val().trim(),
+        $("#comment").val().trim()
+      )
+      .done(function (result) {
+        // reload entries
+        loadEntries();
+      })
+      .error(function (error) {
+        console.log(error);
+      });
   });
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     prepareTemplates();
     loadEntries();
   });
